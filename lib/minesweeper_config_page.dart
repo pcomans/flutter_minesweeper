8f24581dc482200b165341bfaca64471371000a8
Philipp Comans <philipp.comans@gmail.com>
2018-02-18 15:13:54 -0800

    Enable configuring num rows, columns, mines

--- lib/minesweeper_config_page.dart
+++ lib/minesweeper_config_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -8,7 +10,17 @@ import 'minesweeper_game.dart';
 
 typedef OnInitCallback = Function(int numRows, int numCols, int numMines);
 
-class MinesweeperConfigPage extends StatelessWidget {
+class MinesweeperConfigPage extends StatefulWidget {
+  @override
+  _MinesweeperConfigPageState createState() =>
+      new _MinesweeperConfigPageState();
+}
+
+class _MinesweeperConfigPageState extends State<MinesweeperConfigPage> {
+  int _numRows;
+  int _numCols;
+  int _numMines;
+
   @override
   Widget build(BuildContext context) {
     return new StoreConnector<GameBoard, OnInitCallback>(
@@ -31,11 +43,60 @@ class MinesweeperConfigPage extends StatelessWidget {
           body: new Center(
             child: new Column(
               children: <Widget>[
-                new Text("TODO Config goes here"),
+                new Row(
+                  children: <Widget>[
+                    new Text("Rows:"),
+                    new Slider(
+                      value: _numRows.toDouble(),
+                      min: 3.0,
+                      max: 25.0,
+                      onChanged: (newVal) {
+                        setState(() {
+                          _numRows = newVal.floor();
+                          _numMines = min(_numMines, _numCols * _numRows);
+                        });
+                      },
+                    ),
+                    new Text("$_numRows"),
+                  ],
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Text("Columns:"),
+                    new Slider(
+                      value: _numCols.toDouble(),
+                      min: 3.0,
+                      max: 25.0,
+                      onChanged: (newVal) {
+                        setState(() {
+                          _numCols = newVal.floor();
+                          _numMines = min(_numMines, _numCols * _numRows);
+                        });
+                      },
+                    ),
+                    new Text("$_numCols"),
+                  ],
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Text("Mines:"),
+                    new Slider(
+                      value: _numMines.toDouble(),
+                      min: 1.0,
+                      max: _numRows * _numCols * 1.0,
+                      onChanged: (newVal) {
+                        setState(() {
+                          _numMines = newVal.floor();
+                        });
+                      },
+                    ),
+                    new Text("$_numMines"),
+                  ],
+                ),
                 new RaisedButton(
                   child: new Text("Start Game"),
                   onPressed: () {
-                    onInit(12, 9, 16);
+                    onInit(_numRows, _numCols, _numMines);
                     Navigator.of(context).push(getMinesweeperGameRoute());
                   },
                 ),
@@ -45,5 +106,13 @@ class MinesweeperConfigPage extends StatelessWidget {
         );
       },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _numRows = 12;
+    _numCols = 9;
+    _numMines = 16;
   }
 }
