e21a2b86d38a68c2969fc8a502e3a8906710d3c9
Philipp Comans <philipp.comans@gmail.com>
2018-04-20 15:43:09 -0700

    Implement game status face and countdown timer

--- lib/containers/status_bar.dart
+++ lib/containers/status_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:minesweeper/models/game_board.dart';
@@ -16,42 +18,53 @@ class StatusBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Stream<Duration> counterStream = new Stream.periodic(new Duration(milliseconds: 200), (_) {
+      return new DateTime.now().difference(startTime);
+    });
     return SizedBox(
-      height: 80.0,
+      height: 60.0,
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           new Text(flagsRemaining.toString()),
-          new Text(status.toString()),
-          new Text(startTime.toIso8601String()),
+          new GameStatusIndicator(status: status),
+          new StreamBuilder<Duration>(
+            stream: counterStream,
+            builder: (context, snap) {
+              if (snap.hasData) {
+                return new Text(snap.data.toString().split(".").first);
+              }
+              return new Text("--");
+            },
+          ),
         ],
       ),
     );
   }
 }
 
-// class GameStatusIndicator extends StatelessWidget {
-//   final GameStatus status;
+class GameStatusIndicator extends StatelessWidget {
+  final GameStatus status;
 
-//   const GameStatusIndicator({Key key, this.status}) : super(key: key);
+  const GameStatusIndicator({Key key, this.status}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     String face = "";
-//     switch (status) {
-//       case GameStatus.ongoing:
-//         face = "🙂";
-//         break;
-//       case GameStatus.won:
-//         face = "😀";
-//         break;
-//       case GameStatus.lost:
-//         face = "😵";
-//         break;
-//     }
-//     return SizedBox(
-//       width: 5.0,
-//       child: new Text(status.toString()),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    String face = "";
+    switch (status) {
+      case GameStatus.ongoing:
+        face = "🙂";
+        break;
+      case GameStatus.won:
+        face = "😀";
+        break;
+      case GameStatus.lost:
+        face = "😵";
+        break;
+    }
+    return SizedBox(
+      width: 5.0,
+      child: new Text(face),
+    );
+  }
+}
